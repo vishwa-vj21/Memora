b2c18b6414501bd64c35ef27c596b7fcc5ed4f94
vishwa.vj <iamvishwajith.s@gmail.com>
2025-08-19 03:39:22 +0530

    React context handling

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,3 +1,5 @@
+import { db } from "@/db";
+import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
 
@@ -5,4 +7,30 @@ export const POST = async (req: NextRequest) => {
   const body = await req.json();
   const { getUser } = getKindeServerSession();
   const user = await getUser();
+
+  const userId = user?.id;
+  if (!userId) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  const { fileId, message } = SendMessageValidator.parse(body);
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileId,
+      userId,
+    },
+  });
+  if (!file) {
+    return new Response("File not found", { status: 404 });
+  }
+
+  await db.message.create({
+    data: {
+      text: message,
+      isUserMessage: true,
+      userId,
+      fileId,
+    },
+  });
 };
